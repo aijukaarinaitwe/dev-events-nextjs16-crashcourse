f38fbbbc3dcdf12e7e1e26b10e45eef956bf3af6
aijukaarinaitwe <aijuka.arinaitwe@iotec.io>
2026-07-03 00:49:21 +0300

    Fix typescript parameter check on authGET and authPOST route handlers

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -4,22 +4,22 @@ import { NextRequest } from "next/server";
 
 const { GET: authGET, POST: authPOST } = toNextJsHandler(auth);
 
-export async function GET(request: NextRequest, context: any) {
+export async function GET(request: NextRequest) {
     const url = new URL(request.url);
     if (url.pathname.endsWith("/")) {
         url.pathname = url.pathname.slice(0, -1);
         const modifiedRequest = new NextRequest(url, request);
-        return authGET(modifiedRequest, context);
+        return authGET(modifiedRequest);
     }
-    return authGET(request, context);
+    return authGET(request);
 }
 
-export async function POST(request: NextRequest, context: any) {
+export async function POST(request: NextRequest) {
     const url = new URL(request.url);
     if (url.pathname.endsWith("/")) {
         url.pathname = url.pathname.slice(0, -1);
         const modifiedRequest = new NextRequest(url, request);
-        return authPOST(modifiedRequest, context);
+        return authPOST(modifiedRequest);
     }
-    return authPOST(request, context);
+    return authPOST(request);
 }
